8524934c73b6ceadc6ed6f8413e980d6fdce1c89
parinyad123 <sci.equ@gmail.com>
2019-12-11 14:35:11 +0700

    mysql create table

--- mysql/Create-tb-python.py
+++ mysql/Create-tb-python.py
@@ -1,5 +1,15 @@
 import mysql.connector
-mydb=mysql.connector.connect(host="localhost",user="root", password="123qaz")
+mydb=mysql.connector.connect(host="localhost",
+                             user="root",
+                             password="123qaz",
+                             database="harshdb")
 
 mycursor=mydb.cursor()
-mycursor.execute("Create table employee(name varchar(200), sal int(20))")
+
+# Create table of database harshdb
+#mycursor.execute("Create table employee(name varchar(200), salary int(20))")
+
+mycursor.execute("show tables")
+
+for tb in mycursor:
+    print(tb)
